f683b389a62cb26217f9891851e3f6b80829a57d
ahmadullah <nikzadahmadullah@gmail.com>
2025-07-23 20:05:02 +0200

    something pull staff added

--- next.config.ts
+++ next.config.ts
@@ -108,13 +108,6 @@ const nextConfig: NextConfig = {
             test: /[\\/](google-analytics|analytics)[\\/]/,
             priority: 15,
           },
-          // Common vendor chunks
-          vendor: {
-            name: 'vendor',
-            chunks: 'all',
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10,
-          },
         },
       },
     };
